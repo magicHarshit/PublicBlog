cfd91e577fe74d727cdce4bb560ac01bde565c7d
magicHarshit <harshit.py@gmail.com>
2013-12-11 16:21:05 +0530

    Update models.py

--- post/models.py
+++ post/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from ckeditor.fields import RichTextField
+import const
 
 class Article(models.Model):
     user = models.ForeignKey(User)
