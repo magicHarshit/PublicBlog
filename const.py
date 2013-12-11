c527c1452fa810535b91988807712c9e316fb487
magicHarshit <harshit.py@gmail.com>
2013-12-11 16:20:05 +0530

    Create const.py

---
+++ const.py
@@ -0,0 +1,15 @@
+from django.utils.translation import ugettext_lazy as _
+import re
+
+#todo: customize words
+CLOSE_REASONS = (
+    (1, _('Duplicate Article')),
+    (2, _('Article is off-topic or not relevant')),
+    (3, _('too subjective and argumentative')),
+    (4, _('not a real Article')),
+    (5, _('Duplicate Article')),
+    (6, _('Article is not relevant or outdated')),
+    (7, _('Article contains offensive or malicious remarks')),
+    (8, _('spam or advertising')),
+    (9, _('too localized')),
+)
